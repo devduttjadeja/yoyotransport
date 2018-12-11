1d4d5acdc79e11b663903df5b7f0b4af8b556546
devdutt.jadeja <devdutt.jadeja@P3A-B1Y0T72.dir.svc.accenture.com>
2018-12-11 20:29:52 +0530

    classpath: not needed in @PropertiesSource

--- src/main/java/com/example/mykong/HelloMessageService.java
+++ src/main/java/com/example/mykong/HelloMessageService.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service // @Component @Repository @Service all are used to inject by using @Autowired in main class
-@PropertySource("classpath:/properties/myproperties.properties")
+@PropertySource("/properties/myproperties.properties")
 public class HelloMessageService {
 
 	@Value("${name}") // the value is injected from myproperties.properties file only if the properties are NOT define in app.properties
