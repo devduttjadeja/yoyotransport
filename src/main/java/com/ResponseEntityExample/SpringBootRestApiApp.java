f9d8993e610eb647cf1a1b4bc090c15ef38ffd84
devdutt.jadeja <devdutt.jadeja@P3A-B1Y0T72.dir.svc.accenture.com>
2018-12-27 19:08:13 +0530

    change in class SpringBootRestApiApp

--- src/main/java/com/ResponseEntityExample/SpringBootRestApiApp.java
+++ src/main/java/com/ResponseEntityExample/SpringBootRestApiApp.java
@@ -1,13 +1,26 @@
 package com.ResponseEntityExample;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
  
  
 @SpringBootApplication// same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class SpringBootRestApiApp {
  
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootRestApiApp.class, args);
+        
+    	ApplicationContext applicationContext = SpringApplication.run(SpringBootRestApiApp.class, args);
+        
+    	String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+    	
+    	Arrays.sort(beanDefinitionNames);
+        
+    	for (String beanName : beanDefinitionNames) {
+            System.out.println(beanName);
+        }
+    	
     }
 }
