52eb2a3133b7de6aa50e0cfdca314af65dc5ca0c
devdutt.jadeja <devdutt.jadeja@P3A-B1Y0T72.dir.svc.accenture.com>
2019-02-08 11:05:10 +0530

    new example added

---
+++ src/main/java/practice/Final_List_Example.java
@@ -0,0 +1,29 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Final_List_Example {
+
+	public static void main(String[] args) {
+
+		List<String> arrayList = new ArrayList<String>();
+		
+		System.out.println(arrayList.size());
+		
+		arrayList.add("1");
+		arrayList.add("2");
+		arrayList.add("3");
+		arrayList.add("4");
+		arrayList.add("5");
+		arrayList.add("6");
+		arrayList.add("7");
+		arrayList.add("8");
+		arrayList.add("9");
+		arrayList.add("10");
+		arrayList.add("11");
+		
+		System.out.println(arrayList.size());
+	}
+
+}
